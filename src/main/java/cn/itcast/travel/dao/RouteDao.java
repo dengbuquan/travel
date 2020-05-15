2b735a1ab06c40204bc9acab0e001f8710b49161
Brian <784091380@qq.com>
2020-05-15 12:23:00 +0800

    实现了搜索功能的后台代码

--- src/main/java/cn/itcast/travel/dao/RouteDao.java
+++ src/main/java/cn/itcast/travel/dao/RouteDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface RouteDao {
     //查询线路的总条数
-    int findTotalCount(int cid);
+    int findTotalCount(int cid,String rname);
     //根据分页的信息查询符合条件的线路集合
-    List<Route> findByPage(int cid,int start,int pageSize);
+    List<Route> findByPage(int cid,int start,int pageSize,String rname);
 }
--- src/main/java/cn/itcast/travel/service/impl/RouteServiceImpl.java
+++ src/main/java/cn/itcast/travel/service/impl/RouteServiceImpl.java
@@ -12,18 +12,18 @@ public class RouteServiceImpl implements RouteService {
     private RouteDao routeDao = new RouteDaoImpl();
 
     @Override
-    public PageBean<Route> pageQuery(int cid, int currentPage, int pageSize) {
+    public PageBean<Route> pageQuery(int cid, int currentPage, int pageSize,String rname) {
         //第一步：创建一个pageBean对象，最后用于返回
         PageBean<Route> pageBean = new PageBean<>();
         //第二步：为pageBean对象的各个属性赋值
         pageBean.setCurrentPage(currentPage);
         pageBean.setPageSize(pageSize);
-        int totalCount = routeDao.findTotalCount(cid);
+        int totalCount = routeDao.findTotalCount(cid,rname);
         pageBean.setTotalCount(totalCount);
         int totalPage = totalCount % pageSize == 0 ? totalCount/pageSize : totalCount/pageSize + 1;
         pageBean.setTotalPage(totalPage);
         int start = (currentPage - 1) * pageSize;
-        List<Route> list = routeDao.findByPage(cid,start,pageSize);
+        List<Route> list = routeDao.findByPage(cid,start,pageSize,rname);
         pageBean.setList(list);
         //第三步：返回pageBean对象
         return pageBean;
